1365baa69c5b7d9d5ca5e3e203529dd6fa089efb
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-02-03 15:44:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const action = urlParams.get('l')
-if (action == dhe29n) {
-  document.write('Hi There!')
-}
+// const queryString = window.location.search;
+// const urlParams = new URLSearchParams(queryString);
