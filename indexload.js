67f5f05c33c61cd5625c25e89b8781f7623483e1
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-02-13 16:21:56 +0000

    Update indexload.js

--- indexload.js
+++ indexload.js
@@ -21,6 +21,9 @@ paramerterTest(action)
 if (action == "lweb-1"){
     location.replace("Links/web-1")
 }
+if (action == "lweb-3097501"){
+    location.replace("https://sites.google.com/view/stuffzez-yt-website/links/hidden/hfkw03jnc_fj03sk/309jiojiofwf_aj390ajf/jf0402_fk30sk")
+}
 if (action == "rl-1"){
     location.replace("index")
 }
