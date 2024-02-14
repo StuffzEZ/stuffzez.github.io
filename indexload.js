bbd377bc57ca52fe1b3265733073c8d223ac79b3
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-02-14 10:44:55 +0000

    Update indexload.js

--- indexload.js
+++ indexload.js
@@ -22,11 +22,14 @@ if (action == "lweb-1"){
     location.replace("Links/web-1")
 }
 if (action == "lweb-3097501"){
-    location.replace("https://sites.google.com/view/stuffzez-yt-website/links/hidden/hfkw03jnc_fj03sk/309jiojiofwf_aj390ajf/jf0402_fk30sk")
+    location.replace("404")
 }
 if (action == "rl-1"){
     location.replace("index")
 }
 if (action == "rl-2"){
     location.replace("index.html?act=lweb-1")
+}
+if (action == "fdj93qjf930"){
+    console.log("Loaded index.html?act=fdj93qjf930")
 }
