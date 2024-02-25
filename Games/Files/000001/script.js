f8877c37e52c749e3486b7e99646fef011df53b5
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-02-25 12:54:51 +0000

    Update script.js

--- Games/Files/000001/script.js
+++ Games/Files/000001/script.js
@@ -1,1 +1,14 @@
+var b = document.querySelector("clickBTN");
+b.addEventListener("click",change);
+function change()
+{
+    var i = Math.floor(Math.random()*500)+1;
+    var j = Math.floor(Math.random()*500)+1;
+    b.style.left = i+"px";
+    b.style.top = j+"px";
+}
 
+// Add a mouseover event listener
+clickBTN.addEventListener('mouseover', () => {
+  change()
+});
