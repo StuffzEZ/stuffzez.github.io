f4a835b35201caed4507f72cdd9fd30865b9c895
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-03-05 19:21:18 +0000

    Update script.js

--- Games/Files/000001/script.js
+++ Games/Files/000001/script.js
@@ -1,18 +1,3 @@
-var b = document.querySelector("clickBTN");
-b.addEventListener("click",change);
-function change()
-{
-    var i = Math.floor(Math.random()*500)+1;
-    var j = Math.floor(Math.random()*500)+1;
-    b.style.left = i+"px";
-    b.style.top = j+"px";
-}
-
-// Add a mouseover event listener
-clickBTN.addEventListener('mouseover', () => {
-  change()
-});
-
 function updatesSHOW() {
-    window.location.replace("https://github.com/StuffzEZ/stuffzez.github.io/blob/main/Updates.md")
+    loaction.replace('https://stuffzez.github.io/Games/Files/000001/ButtonClicked')
 }
