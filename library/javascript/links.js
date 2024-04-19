e4c4f099050f865a3f0ae9d8852b7b16f70aa252
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-04-19 18:06:51 +0100

    Update links.js

--- library/javascript/links.js
+++ library/javascript/links.js
@@ -5,5 +5,5 @@ if (currentUrl == "https://stuffzez.github.io/l/cj0k"){
 }
 
 if (currentUrl == "https://stuffzez.github.io/l/dl30"){
-  location.replace("https://stuffzez.github.io/other/welcome")
+  location.replace("https://stuffzez.github.io/other/welcome?prev=https://youtube.com/@YT.StuffzEZ")
 }
