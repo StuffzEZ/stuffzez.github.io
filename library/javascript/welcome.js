20d46d29983799ca12ff822b1ba7099083bb5515
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-04-19 17:55:22 +0100

    Update welcome.js
    Removed Feature Where 404 Comes Up If No Parameters Defined

--- library/javascript/welcome.js
+++ library/javascript/welcome.js
@@ -2,13 +2,6 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const previouspage = urlParams.get('prev')
 
-function paramerterTest(parameter) {
-    if (parameter !== undefined){
-        location.replace("https://stuffzez.github.io/404")
-    }
-}
-
 function goBack(){
-    paramerterTest(previouspage)
     location.replace(previouspage)
 }
