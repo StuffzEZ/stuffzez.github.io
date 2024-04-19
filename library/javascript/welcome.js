e0f117923da5c8baa057a85136123b05038fe14b
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-04-19 18:01:40 +0100

    Update welcome.js
    Fixed Bugs

--- library/javascript/welcome.js
+++ library/javascript/welcome.js
@@ -2,6 +2,13 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const previouspage = urlParams.get('prev')
 
+function paramerterTest(parameter) {
+    if (parameter !== undefined){
+        location.replace("home")
+    }
+}
+
 function goBack(){
+    paramerterTest(previouspage)
     location.replace(previouspage)
 }
