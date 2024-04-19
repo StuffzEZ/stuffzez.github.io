fbfa8df6e139cdc6fe0189f1f6bcd5dff88c1a87
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-04-19 16:41:44 +0100

    Update links.js

--- library/javascript/links.js
+++ library/javascript/links.js
@@ -3,3 +3,7 @@ const currentUrl = window.location.href;
 if (currentUrl == "https://stuffzez.github.io/l/cj0k"){
   location.replace("https://youtube.com/@YT.StuffzEZ")
 }
+
+if (currentUrl == "https://stuffzez.github.io/l/dl30"){
+  location.replace("https://stuffzez.github.io/other/welcome.html")
+}
